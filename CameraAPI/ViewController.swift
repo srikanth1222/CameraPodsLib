0645044c67984099ed92dd4399f3d822163d4490
Srikanth <srikanth.vidapu@piing.com.sg>
2020-06-13 13:18:43 +0530

    first commit

---
+++ CameraAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraAPI
+//
+//  Created by Veedepu Srikanth on 13/06/20.
+//  Copyright © 2020 Piing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CameraPodsLib/CameraPodsLib.h
@@ -0,0 +1,19 @@
+//
+//  CameraPodsLib.h
+//  CameraPodsLib
+//
+//  Created by Veedepu Srikanth on 13/06/20.
+//  Copyright © 2020 Piing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CameraPodsLib.
+FOUNDATION_EXPORT double CameraPodsLibVersionNumber;
+
+//! Project version string for CameraPodsLib.
+FOUNDATION_EXPORT const unsigned char CameraPodsLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraPodsLib/PublicHeader.h>
+
+
